3b9c67955585fcdeb3e47f75f80b5cf84b304bbe
Frierensama <rudeusgreyrat.zeta002@gmail.com>
2024-01-22 01:52:21 +0530

    Vector size

---
+++ Stl-Lib/rough.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>>t;
+
+    t[0].push_back(10);
+     t[0].push_back(10);
+
+ t[1].push_back(10);
+ t[2].push_back(10);
+ t[3].push_back(10);
+cout<<t[0].size();
+cout<<t.size();
+
+}
